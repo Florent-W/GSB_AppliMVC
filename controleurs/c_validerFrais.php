8b1a37fb954fde1e2ba260ef95de7d689fb46a55
Florent <florent.weltmann@gmail.com>
2018-02-02 13:54:51 +0100

    Première implémentation des noms des visiteurs

--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -9,10 +9,33 @@
  * @author    Florent WELTMANN <florent.weltmann@gmail.com>
  * @version   GIT: <0>
  */
+?>
+<select name="visiteur" id="visiteur">
+<?php
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=gsb_frais', 'root', '');
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+}
+$reponse = $bdd->query('SELECT * FROM visiteur');
 
+while ($donnees = $reponse->fetch())
+{
+    ?>
+           <option value=" <?php echo $donnees['nom']; ?>"> <?php echo $donnees['prenom']; ?></option>
+<?php
+}
+ 
+$reponse->closeCursor();
+ 
+?>
+</select>
+<?php 
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 $idVisiteur = $_SESSION['idVisiteur'];
-
 $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
 // Afin de sélectionner par défaut le dernier mois dans la zone de liste
 // on demande toutes les clés, et on prend la première,
@@ -20,5 +43,4 @@ $lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
 $lesCles = array_keys($lesMois);
 $moisASelectionner = $lesCles[0];
 include 'vues/v_listeVisiteurs.php';
-           
 ?>
