c4c4bdc871f9083b930679f38d5adfd931d2b707
Florent <florent.weltmann@gmail.com>
2018-02-02 20:56:43 +0100

    Première implémentation du javascript pour tester la récupération des valeurs

--- vues/v_listeVisiteurs.php
+++ vues/v_listeVisiteurs.php
@@ -1,5 +1,5 @@
 
-
+ 
 <div class="row">
     <div class="col-md-4">
         <div>Sélectionner un mois : </div>
@@ -7,7 +7,8 @@
      <div class="col-md-4">
         <form action="index.php?uc=etatFrais&action=voirEtatFrais" 
               method="post" role="form">
-              <select name="visiteur" id="visiteur">
+              <select id="lstMois" name="lstMois" onChange="javascript:document.getElementById('visiteur').value = this.value;">
+              
 <?php
 try
 {
@@ -31,8 +32,7 @@ $reponse->closeCursor();
 ?>
 </select>
                  <label for="lstMois" accesskey="n">Mois : </label>
-<select id="lstMois" name="lstMois">
-
+<select id="lstMois" name="lstMois" onChange="javascript:document.getElementById('mois').value = this.value;">
  <?php
  foreach ($lesMois as $unMois) {
 $mois = $unMois['mois'];
@@ -51,10 +51,43 @@ if ($mois == $moisASelectionner) {
                         }
                     }
                     ?>    </select>
-            <input id="ok" type="submit" value="Valider" class="btn btn-success" 
-                   role="button">
-            <input id="annuler" type="reset" value="Effacer" class="btn btn-danger" 
-                   role="button">
+        </form>
+    </div>
+</div>
+<input type="text" name="visiteur" value="" id="visiteur"/> <!-- test pour voir si on peut prendre la valeur selectionner -->
+<input type="text" name="mois" value="" id="mois"/>
+ <?php 
+ $lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $mois);
+ ?>
+<div class="row">    
+    <h2>Renseigner ma fiche de frais du mois 
+        <?php echo $numMois . '-' . $numAnnee ?>
+    </h2>
+    <h3>Eléments forfaitisés</h3>
+    <div class="col-md-4">
+        <form method="post" 
+              action="index.php?uc=gererFrais&action=validerMajFraisForfait" 
+              role="form">
+            <fieldset>       
+                <?php
+                foreach ($lesFraisForfait as $unFrais) {
+                    $idFrais = $unFrais['idfrais'];
+                    $libelle = htmlspecialchars($unFrais['libelle']);
+                    $quantite = $unFrais['quantite']; ?>
+                    <div class="form-group">
+                        <label for="idFrais"><?php echo $libelle ?></label>
+                        <input type="text" id="idFrais" 
+                               name="lesFrais[<?php echo $idFrais ?>]"
+                               size="10" maxlength="5" 
+                               value="<?php echo $quantite ?>" 
+                               class="form-control">
+                    </div>
+                    <?php
+                }
+                ?>
+                <button class="btn btn-success" type="submit">Ajouter</button>
+                <button class="btn btn-danger" type="reset">Effacer</button>
+            </fieldset>
         </form>
     </div>
 </div>
