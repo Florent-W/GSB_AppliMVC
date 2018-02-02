c5ecda6d32dc5c0505af25ec4ef3bbf28239a607
Florent <florent.weltmann@gmail.com>
2018-02-02 19:03:58 +0100

    Continuation de la page valider frais

--- controleurs/c_validerFrais.php
+++ controleurs/c_validerFrais.php
@@ -9,30 +9,8 @@
  * @author    Florent WELTMANN <florent.weltmann@gmail.com>
  * @version   GIT: <0>
  */
-?>
-<select name="visiteur" id="visiteur">
-<?php
-try
-{
-    $bdd = new PDO('mysql:host=localhost;dbname=gsb_frais', 'root', '');
-}
-catch(Exception $e)
-{
-    die('Erreur : '.$e->getMessage());
-}
-$reponse = $bdd->query('SELECT * FROM visiteur');
+?>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 
-while ($donnees = $reponse->fetch())
-{
-    ?>
-           <option value=" <?php echo $donnees['nom']; ?>"> <?php echo $donnees['prenom']; ?></option>
-<?php
-}
- 
-$reponse->closeCursor();
- 
-?>
-</select>
 <?php 
 $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 $idVisiteur = $_SESSION['idVisiteur'];
--- vues/v_listeVisiteurs.php
+++ vues/v_listeVisiteurs.php
@@ -1,55 +1,56 @@
 
-<div class="row">
-<div class="col-md-4">
-</div>
-<div class="col-md-4">
-<form action="index.php?uc=etatFrais&action=voirEtatFrais"
-    method="post" role="form">
-    <div class="form-group">
-     <select id="lstVisiteur" name="lstVisiteur" class="form-control">
-    <?php
-    foreach ($lesMois as $unMois) {
-        $mois = $unMois['mois'];
-        $numAnnee = $unMois['numAnnee'];
-        $numMois = $unMois['numMois'];
-        if ($mois == $moisASelectionner) {
-            ?>  
-                            <option selected value="<?php echo $mois ?>">
-                                <?php echo $numMois . '/' . $numAnnee ?> </option>
-                            <?php
-                        } else {
-                            ?>
-                            <option value="<?php echo $mois ?>">
-                                <?php echo $numMois . '/' . $numAnnee ?> </option>
-                            <?php
-                        }
-                    }
-                    ?>    
-       </select>
-            </div>
-    <label for="lstMois" accesskey="n">Mois : </label>
-    <select id="lstMois" name="lstMois" class="form-control">
-    <?php
-    foreach ($lesMois as $unMois) {
-        $mois = $unMois['mois'];
-        $numAnnee = $unMois['numAnnee'];
-        $numMois = $unMois['numMois'];
-        if ($mois == $moisASelectionner) {
-            ?>
-                            <option selected value="<?php echo $mois ?>">
-                                <?php echo $numMois . '/' . $numAnnee ?> </option>
-                            <?php
-                        } else {
-                            ?>
-                            <option value="<?php echo $mois ?>">
-                                <?php echo $numMois . '/' . $numAnnee ?> </option>
-                            <?php
-                        }
-                    }
-                    ?>    
 
-                </select>
-            </div>
+<div class="row">
+    <div class="col-md-4">
+        <div>Sélectionner un mois : </div>
+    </div>
+     <div class="col-md-4">
+        <form action="index.php?uc=etatFrais&action=voirEtatFrais" 
+              method="post" role="form">
+              <select name="visiteur" id="visiteur">
+<?php
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=gsb_frais', 'root', '');
+}
+catch(Exception $e)
+{
+    die('Erreur : '.$e->getMessage());
+}
+$reponse = $bdd->query('SELECT * FROM visiteur');
+
+while ($donnees = $reponse->fetch())
+{
+    ?>
+           <option value=" <?php echo $donnees['nom']; ?>"> <?php echo $donnees['nom']; ?> <?php echo $donnees['prenom']; ?></option>
+<?php
+}
+ 
+$reponse->closeCursor();
+ 
+?>
+</select>
+                 <label for="lstMois" accesskey="n">Mois : </label>
+<select id="lstMois" name="lstMois">
+
+ <?php
+ foreach ($lesMois as $unMois) {
+$mois = $unMois['mois'];
+$numAnnee = $unMois['numAnnee'];
+$numMois = $unMois['numMois'];
+if ($mois == $moisASelectionner) {
+            ?>  
+            <option selected value="<?php echo $mois ?>">
+                                <?php echo $numMois . '/' . $numAnnee ?> </option>
+                            <?php
+                        } else {
+                            ?>
+                            <option value="<?php echo $mois ?>">
+                                <?php echo $numMois . '/' . $numAnnee ?> </option>
+                            <?php
+                        }
+                    }
+                    ?>    </select>
             <input id="ok" type="submit" value="Valider" class="btn btn-success" 
                    role="button">
             <input id="annuler" type="reset" value="Effacer" class="btn btn-danger" 
