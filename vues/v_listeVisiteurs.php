799d95db9b0f7c79247705bca1f0668f7df9f82b
Florent <florent.weltmann@gmail.com>
2018-02-02 13:29:44 +0100

    Deuxième liste

--- vues/v_listeVisiteurs.php
+++ vues/v_listeVisiteurs.php
@@ -6,6 +6,27 @@
 <form action="index.php?uc=etatFrais&action=voirEtatFrais"
     method="post" role="form">
     <div class="form-group">
+     <select id="lstVisiteur" name="lstVisiteur" class="form-control">
+    <?php
+    foreach ($lesMois as $unMois) {
+        $mois = $unMois['mois'];
+        $numAnnee = $unMois['numAnnee'];
+        $numMois = $unMois['numMois'];
+        if ($mois == $moisASelectionner) {
+            ?>
+                            <option selected value="<?php echo $mois ?>">
+                                <?php echo $numMois . '/' . $numAnnee ?> </option>
+                            <?php
+                        } else {
+                            ?>
+                            <option value="<?php echo $mois ?>">
+                                <?php echo $numMois . '/' . $numAnnee ?> </option>
+                            <?php
+                        }
+                    }
+                    ?>    
+       </select>
+            </div>
     <label for="lstMois" accesskey="n">Mois : </label>
     <select id="lstMois" name="lstMois" class="form-control">
     <?php
